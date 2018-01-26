4487fdb7ecf76407da7b4cd8492e5f5d3aef7826
Paul Bowen-Huggett <paulhuggett@mac.com>
2018-01-26 15:36:35 +0000

    Add a missing llvm:: name qualification.

--- lib/CodeGen/CodeGenModule.cpp
+++ lib/CodeGen/CodeGenModule.cpp
@@ -4137,7 +4137,7 @@ CodeGenModule::GetAddrOfConstantCFString(const StringLiteral *Literal) {
     GV->setSection("__DATA,__cfstring");
     break;
   case llvm::Triple::Repo:
-    report_fatal_error ("CFString is not yet supported by the Repo");
+    llvm::report_fatal_error ("CFString is not yet supported by the Repo");
   }
   Entry.second = GV;
 
